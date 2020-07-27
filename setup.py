d03af37a7be94f58c247bdf4a83532e93eccae78
Rajeev Kumar <2986.rajeev@gmail.com>
2020-07-27 15:57:00 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name='python-binary-memcached',
+    version='0.0.1',
+    description='A simple python rest client',
+    url='https://github.com/sh3r1ll/pyrestclient',
+    packages=['restclient'],
+    classifiers=[
+    ],
+    install_requires=[
+        'requests'
+    ]
+)
